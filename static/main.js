cdc150eeb3694baf4b9ed4263dd1903b735245bd
BrianHughesdev <djgnarfox@gmail.com>
2025-12-10 14:48:49 -0500

    Delete static/main.js

--- static/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const flightForm = document.querySelector("form[action='/travel/']");
-    if (flightForm) {
-        flightForm.addEventListener("submit", (e) => {
-            console.log("Flight form submitted!");
-        });
-    }
-});
